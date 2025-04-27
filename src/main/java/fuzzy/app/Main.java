130d196d26a1b30077df7fe50fdb6df34e4a5506
JoaStacker <joessar99@gmail.com>
2025-04-27 13:08:07 -0300

    running.

---
+++ src/main/java/fuzzy/app/Main.java
@@ -0,0 +1,37 @@
+package fuzzy.app;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Load from 'FCL' file
+        String fileName = "fcl/tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) {
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        // Show
+//        JFuzzyChart.get().chart(functionBlock);
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+//        Variable tip = functionBlock.getVariable("tip");
+//        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
+    }
+}
